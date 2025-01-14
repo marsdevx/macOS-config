e5840056fc2a15831079088aa77350f38b75ffe1
GNUS <60074926+theMark001@users.noreply.github.com>
2025-01-14 12:50:30 +0100

    fix: install plugin, better config in iterm.sh

--- iterm.sh
+++ iterm.sh
@@ -11,42 +11,31 @@
 
 #!/bin/bash
 
+# Install Iterm
+brew install --cask iterm2
+
 # Install Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Install Power Level 10k
 git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 
-# Append zshrc
-cat << EOF >> ~/.zshrc
-# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
-# Initialization code that may require console input (password prompts, [y/n]
-# confirmations, etc.) must go above this block; everything else may go below.
-if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
-  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
-fi
+# Install Zsh Syntax Highlighting Plugin
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-# Consolidated PATH
+# Rewrite zshrc
+cat << EOF > ~/.zshrc
+export ZSH="$HOME/.oh-my-zsh"
+
 export PATH="$HOME/bin:$HOME/.local/bin:/usr/local/bin:$HOME/.rvm/bin:$PATH"
 export PATH="$PATH:/opt/homebrew/bin"
 
-# Path to your Oh My Zsh installation.
-export ZSH="$HOME/.oh-my-zsh"
-
-# Set name of the theme to load --- if set to "random", it will
-# load a random theme each time Oh My Zsh is loaded, in which case,
-# to know which specific one was loaded, run: echo $RANDOM_THEME
-# See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
 ZSH_THEME="powerlevel10k/powerlevel10k"
 
-# Plugins
-plugins=(git zsh-syntax-highlighting web-search)
+plugins=(git zsh-syntax-highlighting)
 
 source $ZSH/oh-my-zsh.sh
-
-# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
-[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
 EOF
 
-# Restart zshrc
+# Update zshrc
 source ~/.zshrc
